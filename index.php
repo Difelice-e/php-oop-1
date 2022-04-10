cb3c71b0e708488df302a1857fe526a9a5599336
Difelice-e <difelice.emanuele@gmail.com>
2022-04-10 15:06:01 +0200

    creato movie prova e card

--- index.php
+++ index.php
@@ -1,4 +1,44 @@
 <?php
 require_once __DIR__.'/movie.php';
+
+$prova = new movie(
+    'example.jpg',
+    'prova',
+    'it',
+    'prova',
+    '01 gennaio 2000'
+  );
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP-OOP-1</title>
+</head>
+<body>
+  <div class="container">
+    <div class="card_wrapper">
+      <figure>
+        <img class="card_img" src="<?php echo $prova->poster ?>" alt="">
+      </figure>
+      
+      <h3 class="card-title">
+        <?php echo $prova->title ?>
+      </h3>
+
+      <div class="card_info">
+        <?php 
+         echo $prova->language     
+         echo $prova->director     
+         echo $prova->releaseDate 
+        ?>    
+      </div>
+    </div>
+
+  </div>
+</body>
+</html>
 
