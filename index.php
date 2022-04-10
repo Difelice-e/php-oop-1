8de111b999da7ac6d3796dc76356d8ee5fe6aca6
Difelice-e <difelice.emanuele@gmail.com>
2022-04-10 14:58:13 +0200

    creata classe movie

--- index.php
+++ index.php
@@ -1,2 +1,20 @@
-<?php ?>
+<?php 
+
+class movie {
+    public $poster;
+    public $title;
+    public $language;
+    public $director;
+    public $releaseDate;
+
+    function __construct(string $_poster, string $_title, string $_language, string $_director, string $_releaseDate){
+      $this->poster = $_poster;
+      $this->title = $_title;
+      $this->language = $_language;
+      $this->director = $_director;
+      $this->releaseDate = $_releaseDate;
+    }
+}
+
+?>
 
