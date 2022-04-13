326ee8cd468986a50cf851fe3b4c06d67ec5db52
Difelice-e <difelice.emanuele@gmail.com>
2022-04-13 11:31:22 +0200

    instanziati oggetti movie

--- index.php
+++ index.php
@@ -1,13 +1,29 @@
 <?php
 require_once __DIR__.'/movie.php';
 
-$prova = new movie(
-    'example.jpg',
-    'prova',
-    'it',
-    'prova',
-    '01 gennaio 2000'
-  );
+$fast5 = new movie(
+    'https://m.media-amazon.com/images/I/71R2zUzKb3L._SL1024_.jpg',
+    'Fast & Furios 5',
+    'EN',
+    'Vin Diesel',
+    '2011'
+);
+
+$fast6 = new movie(
+  'https://m.media-amazon.com/images/I/71sag7gEJTL._SL1024_.jpg',
+  'Fast & Furios 6',
+  'EN',
+  'Vin Diesel',
+  '2013'
+); 
+
+$fast7 = new movie(
+  'https://m.media-amazon.com/images/I/61RCCueAUjL._SL1115_.jpg',
+  'Fast & Furios 7',
+  'EN',
+  'Vin Diesel',
+  '2015'
+);
 ?>
 
 <!DOCTYPE html>
@@ -22,19 +38,17 @@ $prova = new movie(
   <div class="container">
     <div class="card_wrapper">
       <figure>
-        <img class="card_img" src="<?php echo $prova->poster ?>" alt="">
+        <img class="card_img" src="<?php echo $fast5->poster ?>" alt="">
       </figure>
       
       <h3 class="card-title">
-        <?php echo $prova->title ?>
+        <?php echo $fast5->title ?>
       </h3>
 
       <div class="card_info">
-        <?php 
-         echo $prova->language     
-         echo $prova->director     
-         echo $prova->releaseDate 
-        ?>    
+        <p><?php echo $fast5->language ?></p>    
+        <p><?php echo $fast5->mainActor  ?></p>   
+        <p><?php echo $fast5->yearsFromRelease() ?> years ago</p>   
       </div>
     </div>
 
--- movie.php
+++ movie.php
@@ -4,15 +4,19 @@ class movie {
     public $poster;
     public $title;
     public $language;
-    public $director;
+    public $mainActor;
     public $releaseDate;
 
-    function __construct(string $_poster, string $_title, string $_language, string $_director, string $_releaseDate){
+    function __construct(string $_poster, string $_title, string $_language, string $_mainActor, string $_releaseDate){
       $this->poster = $_poster;
       $this->title = $_title;
       $this->language = $_language;
-      $this->director = $_director;
+      $this->mainActor = $_mainActor;
       $this->releaseDate = $_releaseDate;
+    }
+
+    public function yearsFromRelease() {
+      return (2022 - $this->releaseDate); 
     }
 }
 
