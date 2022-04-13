d8403321a0e2396f9aced1615d8a503af592012d
Difelice-e <difelice.emanuele@gmail.com>
2022-04-13 11:39:10 +0200

    aggiunto stile

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ $fast6 = new movie(
 ); 
 
 $fast7 = new movie(
-  'https://m.media-amazon.com/images/I/61RCCueAUjL._SL1115_.jpg',
+  'https://pad.mymovies.it/filmclub/2013/10/194/locandina.jpg',
   'Fast & Furios 7',
   'EN',
   'Vin Diesel',
@@ -32,6 +32,7 @@ $fast7 = new movie(
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
   <title>PHP-OOP-1</title>
 </head>
 <body>
@@ -49,6 +50,38 @@ $fast7 = new movie(
         <p><?php echo $fast5->language ?></p>    
         <p><?php echo $fast5->mainActor  ?></p>   
         <p><?php echo $fast5->yearsFromRelease() ?> years ago</p>   
+      </div>
+    </div>
+
+    <div class="card_wrapper">
+      <figure>
+        <img class="card_img" src="<?php echo $fast6->poster ?>" alt="">
+      </figure>
+      
+      <h3 class="card-title">
+        <?php echo $fast6->title ?>
+      </h3>
+
+      <div class="card_info">
+        <p><?php echo $fast6->language ?></p>    
+        <p><?php echo $fast6->mainActor  ?></p>   
+        <p><?php echo $fast6->yearsFromRelease() ?> years ago</p>   
+      </div>
+    </div>
+
+    <div class="card_wrapper">
+      <figure>
+        <img class="card_img" src="<?php echo $fast7->poster ?>" alt="">
+      </figure>
+      
+      <h3 class="card-title">
+        <?php echo $fast7->title ?>
+      </h3>
+
+      <div class="card_info">
+        <p><?php echo $fast7->language ?></p>    
+        <p><?php echo $fast7->mainActor  ?></p>   
+        <p><?php echo $fast7->yearsFromRelease() ?> years ago</p>   
       </div>
     </div>
 
